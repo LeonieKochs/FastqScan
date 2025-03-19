d754cedd82008eabbad63725c42da0864f7df4a7
Leonie Kochs <leonie.kochs@web.de>
2025-03-19 19:06:21 +0100

    Changes with types

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use explain_name::explain_filename;
 use fastq::{avg_err_prob, calculate_phred};
 use clap::Parser;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 mod fastq;
 mod explain_name;
@@ -11,8 +11,8 @@ mod explain_name;
 
 // comand line interface struct
 struct Cli {
-    input1: String,
-    input2:Option<String>,
+    input1: PathBuf,
+    input2:Option<PathBuf>,
 }
 
 
@@ -32,36 +32,36 @@ fn main() {
 
     // Pfad zu einer oder zwei Dateien einlesen
     let args = Cli::parse();
-    println!("1st file probided:{}", args.input1);
+    println!("1st file provided:{}", args.input1.display());
     
     // 
-    if let Some(ref input2) = args.input2 { // wrapped in Option<String>
-        println!("2nd file provided: {}", input2); // input2 is now a regular String
+    if let Some(ref input2) = args.input2 { 
+        println!("2nd file provided: {}", input2.display()); 
     } else {
-        println!("Only one file probided. Likely Single-End.");
+        println!("Only one file provided. Likely Single-End.");
     }
 
 
     // Prüfen ob Datei existiert
     if !Path::new(&args.input1).exists(){
-        println!("Fehler: Die Datei {} existiert nicht.", args.input1); //eprintln! ? equivalent to println! except output goes to io::stderrr instead of io::stdout
+        println!("Fehler: Die Datei {} existiert nicht.", args.input1.display()); //eprintln! ? equivalent to println! except output goes to io::stderrr instead of io::stdout
         return;
     }
 
-    if let Some(ref input2) = args.input2 { // use of moved value !!!
+    if let Some(ref input2) = args.input2 { 
         if !Path::new(input2).exists(){
-            println!("Fehler: Die Datei {} existiert nicht.", input2);
+            println!("Fehler: Die Datei {} existiert nicht.", input2.display());
             return;
         }
     }
 
     // Geben Sie die Informationen über den Dateinamen aus
-    let file_info = explain_filename(&args.input1); //explain_name::explain_filename() ?
+    let file_info:String = explain_filename(&args.input1.to_string_lossy()); // convert PathBuf to String; alternative adapt explain functions
     println!("{}", file_info);
     // 2nd ?
     // explain_filename(&args.input2);
     if let Some(ref input2) = args.input2 {
-        let file_info2:String = explain_filename(&input2);
+        let file_info2:String = explain_filename(&input2.to_string_lossy());
         println!("{}", file_info2);
     }
 
